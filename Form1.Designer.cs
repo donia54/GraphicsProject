6d3a962c50d0f716ab836ce1ec92c1f483af2115
donia <arefdonia491@gmail.com>
2023-11-03 17:32:25 +0200

    DDA

---
+++ Form1.Designer.cs
@@ -0,0 +1,268 @@
+﻿namespace secAss
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btn_Drow = new System.Windows.Forms.Button();
+            this.txt_x2 = new System.Windows.Forms.TextBox();
+            this.txt_y1 = new System.Windows.Forms.TextBox();
+            this.txt_y2 = new System.Windows.Forms.TextBox();
+            this.txt_x1 = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.In = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.X = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Y = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.p_x = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.p_y = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.textBox1.Location = new System.Drawing.Point(491, 122);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(28, 22);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "X1";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.btn_Drow);
+            this.groupBox1.Controls.Add(this.txt_x2);
+            this.groupBox1.Controls.Add(this.txt_y1);
+            this.groupBox1.Controls.Add(this.txt_y2);
+            this.groupBox1.Controls.Add(this.txt_x1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(285, 132);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "inputs";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label4.Location = new System.Drawing.Point(21, 52);
+            this.label4.MaximumSize = new System.Drawing.Size(30, 30);
+            this.label4.MinimumSize = new System.Drawing.Size(20, 20);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(22, 20);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "X2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label3.Location = new System.Drawing.Point(150, 24);
+            this.label3.MaximumSize = new System.Drawing.Size(30, 30);
+            this.label3.MinimumSize = new System.Drawing.Size(20, 20);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(23, 20);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Y1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label2.Location = new System.Drawing.Point(150, 49);
+            this.label2.MaximumSize = new System.Drawing.Size(30, 30);
+            this.label2.MinimumSize = new System.Drawing.Size(20, 20);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(23, 20);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Y2";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label1.Location = new System.Drawing.Point(21, 21);
+            this.label1.MaximumSize = new System.Drawing.Size(30, 30);
+            this.label1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(22, 20);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "X1";
+            // 
+            // btn_Drow
+            // 
+            this.btn_Drow.Location = new System.Drawing.Point(98, 77);
+            this.btn_Drow.Name = "btn_Drow";
+            this.btn_Drow.Size = new System.Drawing.Size(75, 36);
+            this.btn_Drow.TabIndex = 8;
+            this.btn_Drow.Text = "Drow";
+            this.btn_Drow.UseVisualStyleBackColor = true;
+            this.btn_Drow.Click += new System.EventHandler(this.btn_Drow_Click);
+            // 
+            // txt_x2
+            // 
+            this.txt_x2.Location = new System.Drawing.Point(49, 49);
+            this.txt_x2.Name = "txt_x2";
+            this.txt_x2.Size = new System.Drawing.Size(90, 22);
+            this.txt_x2.TabIndex = 7;
+            // 
+            // txt_y1
+            // 
+            this.txt_y1.Location = new System.Drawing.Point(179, 21);
+            this.txt_y1.Name = "txt_y1";
+            this.txt_y1.Size = new System.Drawing.Size(90, 22);
+            this.txt_y1.TabIndex = 6;
+            // 
+            // txt_y2
+            // 
+            this.txt_y2.Location = new System.Drawing.Point(179, 49);
+            this.txt_y2.Name = "txt_y2";
+            this.txt_y2.Size = new System.Drawing.Size(90, 22);
+            this.txt_y2.TabIndex = 5;
+            // 
+            // txt_x1
+            // 
+            this.txt_x1.Location = new System.Drawing.Point(49, 21);
+            this.txt_x1.Name = "txt_x1";
+            this.txt_x1.Size = new System.Drawing.Size(90, 22);
+            this.txt_x1.TabIndex = 4;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.In,
+            this.X,
+            this.Y,
+            this.p_x,
+            this.p_y});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 150);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(289, 288);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // In
+            // 
+            this.In.HeaderText = "In";
+            this.In.MinimumWidth = 6;
+            this.In.Name = "In";
+            this.In.Width = 50;
+            // 
+            // X
+            // 
+            this.X.HeaderText = "X";
+            this.X.MinimumWidth = 6;
+            this.X.Name = "X";
+            this.X.Width = 50;
+            // 
+            // Y
+            // 
+            this.Y.HeaderText = "Y";
+            this.Y.MinimumWidth = 6;
+            this.Y.Name = "Y";
+            this.Y.Width = 50;
+            // 
+            // p_x
+            // 
+            this.p_x.HeaderText = "p_x";
+            this.p_x.MinimumWidth = 6;
+            this.p_x.Name = "p_x";
+            this.p_x.Width = 50;
+            // 
+            // p_y
+            // 
+            this.p_y.HeaderText = "p_y";
+            this.p_y.MinimumWidth = 6;
+            this.p_y.Name = "p_y";
+            this.p_y.Width = 50;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(316, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(458, 426);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btn_Drow;
+        private System.Windows.Forms.TextBox txt_x2;
+        private System.Windows.Forms.TextBox txt_y1;
+        private System.Windows.Forms.TextBox txt_y2;
+        private System.Windows.Forms.TextBox txt_x1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn In;
+        private System.Windows.Forms.DataGridViewTextBoxColumn X;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Y;
+        private System.Windows.Forms.DataGridViewTextBoxColumn p_x;
+        private System.Windows.Forms.DataGridViewTextBoxColumn p_y;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace secAss
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_Drow_Click(object sender, EventArgs e)
+        {
+            dataGridView1.Rows.Clear();
+            pictureBox1.Image = null;
+            Bitmap bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            int x1 = int.Parse(txt_x1.Text);
+            int x2 = int.Parse(txt_x2.Text);
+            int y1 = int.Parse(txt_y1.Text);
+            int y2 = int.Parse(txt_y2.Text);
+            int dx = x2 - x1;
+            int dy = y2 - y1;
+            int steps = Math.Max(Math.Abs(dx), Math.Abs(dy));
+
+            double x_inc = dx /(double) steps;
+            double y_inc = dy /(double)steps;
+            double x = x1;
+            double y = y1;
+            for (int i = 0; i <= steps; i++)
+            {
+                dataGridView1.Rows.Add(i,x, y,Math.Round(x,0),Math.Round(y,0));
+                bmp.SetPixel((int) x,(int) y, Color.Black);
+                x +=x_inc;
+                y += y_inc;
+            }
+
+            pictureBox1.Image = bmp;
+        }
+    }
+}
